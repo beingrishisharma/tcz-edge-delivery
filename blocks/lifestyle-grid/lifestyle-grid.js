60c395003cb133fb78ca5a971d3fc0d114a5eedd
PalakJainTCZ <72863429+PalakJainTCZ@users.noreply.github.com>
2024-02-26 17:19:39 +0530

    lifestyle component updated

---
+++ blocks/lifestyle-grid/lifestyle-grid.js
@@ -0,0 +1,3 @@
+const lifGrid = document.querySelector('.lifestyle-grid > div').classList.add('inner-grid');
+
+const firstCol = document.querySelector('.lifestyle-grid > div').firstElementChild.classList.add('first-col');
